a6268e4e69d32b5ccf052c5d3f537fe2558873e3
yataomes <zhouzheng0404@gmail.com>
2022-08-23 11:07:07 +0800

    11:06

--- CH01/rearrange.c
+++ CH01/rearrange.c
@@ -9,9 +9,33 @@ void rearrange(char *output, char const *input, int n_columns, int const columns
 
 int main(void)
 {
+    int n_columns;
     int columns[MAX_COLS];
     char input[MAX_INPUT];
+    char output[MAX_INPUT];
 
     n_columns = read_column_numbers(columns, MAX_COLS);
+    while(gets(input) != NULL)
+    {
+        printf("Original input : %s\n", input);
+        rearrange(output, input, n_columns, columns);
+        printf("Rearranged line: %s\n", output);
+    }
+    return EXIT_SUCCESS;
+}
 
+int read_column_numbers(int columns[], int max)
+{
+    int num = 0;
+    int ch;
+    while(num < max && scanf("%d", &columns[num]) == 1 && columns[num] >= 0)
+    num += 1;
+    if(num % 2 != 0)
+    {
+        puts("Last column number is not paired.");
+        exit(EXIT_FAILURE);
+    }
+    while((ch = getchar()) != EOF && ch != '\n')
+    ;
+    return num;
 }
