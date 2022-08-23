6b76548ac56f7131b2093383a5a3050d0e090ff0
yataomes <zhouzheng0404@gmail.com>
2022-08-23 15:08:44 +0800

    15:08

--- CH01/rearrange.c
+++ CH01/rearrange.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_COLS 20
 #define MAX_INPUT 1000
 
-int read_column_number(int columns[], int max);
+int read_column_numbers(int columns[], int max);
 void rearrange(char *output, char const *input, int n_columns, int const columns[]);
 
 int main(void)
